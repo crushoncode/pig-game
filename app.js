94daf2ea0b5bf06ae07a0c8f08af2e8647dcf142
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-06-06 16:46:47 +1000

    rolling the dice using event handler

--- app.js
+++ app.js
@@ -18,3 +18,24 @@ activePlayer = 0;
 document.querySelector('.dice').style.display = 'none';
 
 document.getElementById('score-0').textcontent ='0';
+
+
+// How to set up an event handler
+// What a callback function is
+// What an anonymous function is
+// Another way to select elements by ID
+// How to change the image in an <img> element
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    // 1. Random number
+    let dice = Math.floor(Math.random() * 6) + 1;
+    
+    // 2. Display the result
+    let diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+});
+
+
