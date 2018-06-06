8f02952fc37803bdb75b8a08cef4bf95ed9d7d5b
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-06-06 18:41:12 +1000

    display winner

--- app.js
+++ app.js
@@ -4,21 +4,18 @@
 // How to read from the DOM
 // How to change CSS Styles
 
-const scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-// document.querySelector('#current-' +  activePlayer).textContent = dice;
-
-// const x = document.querySelector('#score-0').textContent;
-// console.log(x);
-
 document.querySelector('.dice').style.display = 'none';
 
 document.getElementById('score-0').textcontent ='0';
-
+document.getElementById('score-1').textcontent ='0';
+document.getElementById('current-0').textcontent ='0';
+document.getElementById('current-1').textcontent ='0';
 
 // How to set up an event handler
 // What a callback function is
@@ -27,56 +24,63 @@ document.getElementById('score-0').textcontent ='0';
 // How to change the image in an <img> element
 
 
-document.querySelector('.btn-roll').addEventListener('click', function() {
+    document.querySelector('.btn-roll').addEventListener('click', function() {
     // 1. Random number
     let dice = Math.floor(Math.random() * 6) + 1;
     
     // 2. Display the result
     let diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
 
     // What the ternary operator is
     // How to add, remove and toggle HTML classes
 
     //3. Update the round score IF the rolled number was NOT a 1
     if (dice !== 1) {
-
         roundScore += dice; 
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
         //Next player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
-        roundScore == 0
+        nextPlayer();
+    }
+}); 
+    // How to use functions to correctly apply the DRY principle;
+    // How to think about the game logic like a programmer. 
 
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-
-        // document.querySelector('.player-0-panel').classList.remove('active');
-        // document.querySelector('.player-1-panel').classList.add('active')
-
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-
-        document.querySelector('.dice').style.display = 'none'
-
-        }
-
-         // How to use functions to correctly apply the DRY principle;
-        // How to think about the game logic like a programmer. 
-
-        document.querySelector('.btn-hold').addEventListener('click', function() {
-            // Add CURRENT score to GLOBAL score
-            score[activePlayer] += roundScore;
+    document.querySelector('.btn-hold').addEventListener('click', function() {
+    // Add CURRENT score to GLOBAL score
+    score[activePlayer] += roundScore;
             
-            // Update the UI
-            document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer]
+    // Update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer]
 
-            // Check if player won the game
-
-        })
-
+    // Check if player won the game
+        if (scores[activePlayer >= 20]) {
+            document.querySelector('#name-' + activePlayer).textContent = "Winner!";
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('winner');
+        } else {
+            //Next player
+            nextPlayer();
+            }
+        });
        
-});
-
-
+        function nextPlayer() {
+            //Next player
+            activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+            roundScore = 0;
+    
+            document.getElementById('current-0').textContent = '0';
+            document.getElementById('current-1').textContent = '0';
+    
+            document.querySelector('.player-0-panel').classList.toggle('active');
+            document.querySelector('.player-1-panel').classList.toggle('active');
+    
+            // document.querySelector('.player-0-panel').classList.remove('active');
+            // document.querySelector('.player-1-panel').classList.add('active')
+    
+            document.querySelector('.dice').style.display = 'none'
+        }
+    
