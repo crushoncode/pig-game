552fbb59f3be8737b0d9a307685bf39f24dcdc3b
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-06-06 16:41:24 +1000

    manipulated the DOM

---
+++ app.js
@@ -0,0 +1,20 @@
+// How to create our fundamental game variables
+// How to generate a random number
+// How to manipulate the DOM
+// How to read from the DOM
+// How to change CSS Styles
+
+const scores, roundScore, activePlayer;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+// document.querySelector('#current-' +  activePlayer).textContent = dice;
+
+// const x = document.querySelector('#score-0').textContent;
+// console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textcontent ='0';
